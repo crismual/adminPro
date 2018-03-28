c6d8195e1d0c6e5dbd28cab70daa4dc2b29a2881
CristianMunoz <cristian.munoz@pragma.com.co>
2018-03-28 09:29:15 -0500

    final progress

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 
 
 @Component({
@@ -7,7 +7,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styles: []
 })
 export class IncrementadorComponent implements OnInit {
-
+  @ViewChild('txtProgress') txtProgress: ElementRef;
   @Input() leyenda: string = 'LEYENDA';
   @Input() porcentaje: number = 50;
 
@@ -21,9 +21,24 @@ export class IncrementadorComponent implements OnInit {
     console.log('leyenda', this.leyenda);
     console.log('leyenda', this.porcentaje);
   }
+  onChange(newValue: number) {
+
+
+    if (newValue >= 100) {
+      this.porcentaje = 100;
+    } else if (newValue <= 0 ) {
+      this.porcentaje = 0;
+    } else {
+      this.porcentaje = newValue;
+    }
+    this.txtProgress.nativeElement.value = this.porcentaje;
+    this.cambioValor.emit(this.porcentaje);
+  }
+
   cambiarValor( valor: number) {
     this.porcentaje = this.porcentaje + valor;
     this.cambioValor.emit(this.porcentaje);
+    this.txtProgress.nativeElement.focus();
     if ( this.porcentaje >= 100) {
     this.porcentaje = 100;
     return;
